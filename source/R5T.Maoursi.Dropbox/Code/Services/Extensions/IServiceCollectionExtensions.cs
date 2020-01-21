a8cce7c8cb761971115a6856bbc2885dfb49461b
MinexAutomation <minex.automation@gmail.com>
2020-01-21 12:05:58 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Maoursi.Dropbox/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bulgaria.UserProfileDirectory;
+using R5T.Kalamaria.Standard;
+using R5T.Lombardy.Standard;
+
+using R5T.Maoursi.Bulgaria;
+
+
+namespace R5T.Maoursi.Dropbox
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOrganizationsDirectoryPathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IOrganizationsDirectoryPathProvider, DropboxOrganizationsDirectoryPathProvider>()
+                .AddOrganizationsDirectoryNameConvention()
+                .AddDropboxDirectoryPathProvider()
+                .AddStringlyTypedPathOperator()
+                ;
+
+            return services;
+        }
+    }
+}
